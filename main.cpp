6fd6f594a4f6d67aa9b2453cf6f7459571230d58
RedMads <o7da.ksa@gmail.com>
2022-04-20 08:11:39 +0300

    write the main code !

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "src/headers/Block.h"
+#include "src/headers/Blockchain.h"
+
+
+int main() {
+
+    std::cout << "\nMining Genesis Block ...\n\n";
+
+
+    Blockchain blockchainObj;
+
+
+    std::vector<std::string> data = {
+
+        "The WORLD is losing",
+        "The WORLD needs a Revolution",
+        "Tell the WORLD i'm coming"
+
+    };
+
+
+    for(std::string line: data) {
+
+        Block b(blockchainObj.getLastBlockHash(), line, time(0));
+
+        std::cout << "Mining Block-" << blockchainObj.getChainLength() << " ...\n\n";
+
+        blockchainObj.addBlock(b);
+        
+    }
+
+
+    blockchainObj.printChain();
+
+    return 0;
+}
